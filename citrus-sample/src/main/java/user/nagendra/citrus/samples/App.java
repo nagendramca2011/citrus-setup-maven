3d0b5b429c112ec2b6f86a32be24d43adbc1c20e
Nagendra Padamara <nagendramca2011@gmail.com>
2017-03-29 22:17:52 +0530

    initial project setup done

---
+++ citrus-sample/src/main/java/user/nagendra/citrus/samples/App.java
@@ -0,0 +1,13 @@
+package user.nagendra.citrus.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
